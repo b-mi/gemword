e713058e9ac22f9229c56c59fa1b112a36ac85bb
code@arxa.eu <code@arxa.eu>
2025-11-30 18:14:37 +0100

    build

--- build.cmd
+++ build.cmd
@@ -2,6 +2,10 @@
 call npm version patch
 call ng build -c production --base-href .
 del gemword\dist\gemword\browser\config.json 
-rem echo key to copy to IIS
+echo key to copy to ISP
 pause
+rclone sync ./dist/gemword/browser arxa:/subdoms/gemword/wwwroot/ --progress --exclude "media/**" --exclude "config.json" --dry-run
+
+REM rclone delete arxa:/www/ --max-depth 1 --filter "+ *.js" --filter "+ *.css" --filter "- *" -v 
+pause 
 rem call copyto.cmd
